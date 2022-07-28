a8403d12039a123ff01a8ff84944ca41221a395e
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-07-28 20:03:32 -0300

    Boss

---
+++ Assets/Scrips/Boss/Proyectil.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : Enemy
+{
+    [SerializeField] float KeyVelocity;
+    public override void Update()
+    {
+        base.Update();
+        ToDie = false;
+        
+    }
+    public override void Mov()
+    {
+        this.transform.Translate(new Vector3(-KeyVelocity* Time.deltaTime, 0),Space.World);
+    }
+}
